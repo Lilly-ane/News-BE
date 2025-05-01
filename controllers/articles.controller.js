8c10ae05d826210e4b08da9d93436050f9ae8c30
Liliana Vaciu <93165436+Lilly-ane@users.noreply.github.com>
2025-05-01 13:01:01 +0100

    Update articles.controller.js

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,21 +1,19 @@
+const { selectArticleById } = require("../models/articles.model");
+
 const db = require("../db/connection");
 
 const getArticleById = (req, res, next) => {
   const { article_id } = req.params;
 
-  db.query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
-    .then((result) => {
-      const article = result.rows[0];
-
+  selectArticleById(article_id)
+    .then((article) => {
       if (!article) {
         return next({ status: 404, msg: "Article not found" });
       }
-
       res.status(200).send({ article });
     })
-    .catch((err) => {
-      next(err);
-    });
+    .catch(next);
 };
 
 module.exports = { getArticleById };
+
