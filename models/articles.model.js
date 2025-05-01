d720f3d9766087b420d8a02782558e9a457762bf
Liliana Vaciu <93165436+Lilly-ane@users.noreply.github.com>
2025-05-01 13:01:21 +0100

    Update articles.model.js

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,11 +1,19 @@
 const db = require("../db/connection");
 
+
 const selectArticleById = (articleId) => {
+  if (isNaN(articleId)) {
+    return Promise.reject({ status: 400, msg: "Invalid article_id" });
+  }
+
   return db
-    .query(`SELECT * FROM articles WHERE article_id = $1`, [articleId])
+    .query("SELECT * FROM articles WHERE article_id = $1;", [articleId])
     .then((result) => {
-      return result.rows[0];
+      return result.rows[0]; // fie articolul, fie undefined
     });
 };
 
 module.exports = { selectArticleById };
+
+
+
