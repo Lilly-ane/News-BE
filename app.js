0e4ee9c995c848187ec094a0a85118f14aeb0f89
Lilly-ane <lucalavinia23@yahoo.com>
2025-05-14 15:55:31 +0100

    6 post-api-articles-articles_id-comments

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const { getApi } = require("./controllers/api.controller");
 const { getTopics } = require("./controllers/topics.controller");
 const { getArticleById,getArticles } = require("./controllers/articles.controller");
-const { getCommentsByArticleId } = require("./controllers/comments.controller");
+const { getCommentsByArticleId, postCommentForArticle} = require("./controllers/comments.controller");
 app.use(express.json());
 
 app.get("/api", getApi);
@@ -12,6 +12,7 @@ app.get("/api/topics", getTopics);
 app.get("/api/articles", getArticles)
 app.get("/api/articles/:article_id", getArticleById);
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId)
+app.post("/api/articles/:article_id/comments", postCommentForArticle)
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "Route not found" });
 });
