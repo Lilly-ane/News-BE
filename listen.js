4789b012459f52caf5944d661f9df7a64d337c0b
Lilly-ane <lucalavinia23@yahoo.com>
2025-05-15 21:39:05 +0100

    supabase settings done

--- listen.js
+++ listen.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.listen(9090, () => {
-    console.log("Server is listening on port 9090...");
-})
+const app = require("./app.js");
+const { PORT = 9090 } = process.env;
+
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
