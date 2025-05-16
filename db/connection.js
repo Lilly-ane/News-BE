13c88220a9dbd4d1c4e136411e7dc02f6113e567
Lilly-ane <lucalavinia23@yahoo.com>
2025-05-16 15:26:19 +0100

    update hosting

--- db/connection.js
+++ db/connection.js
@@ -4,12 +4,10 @@ const ENV = process.env.NODE_ENV || 'development'
 
 require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
-const db = new Pool();
 
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
-} else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
 const config = {};
@@ -19,4 +17,3 @@ if (ENV === "production") {
   config.max = 2;
 }
 module.exports = new Pool(config);
-module.exports = db;
--- listen.js
+++ listen.js
@@ -1,6 +1,3 @@
-const express = require("express");
-const app = express();
-
 const app = require("./app.js");
 const { PORT = 9090 } = process.env;
 
